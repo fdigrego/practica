62f670691c4740b72a5a4686c2f66cc461fa9cb9
fdigrego <fabian.digregorio@gmail.com>
2024-08-02 20:20:26 -0300

    APPCoder c/boost ok

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
-    path('/', views.inicio),
+    path('', views.inicio),
     path('cursos/', views.cursos),
     path('profesores/', views.profesores),
     path('estudiantes/', views.estudiantes),
