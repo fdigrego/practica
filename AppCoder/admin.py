91e29c152e51d0ac5b0db15339d794aa430c50cb
fdigrego <fabian.digregorio@gmail.com>
2024-08-02 21:27:24 -0300

    superuser created

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profesor, Entregable, Estudiante, Curso
 
 # Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Entregable)
+admin.site.register(Estudiante)
+admin.site.register(Curso)
